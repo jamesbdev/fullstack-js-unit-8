4e28fc217caaf6bb60eb05d9a2513f836994b27d
James Bremner <j.bremner@ymail.com>
2024-05-30 15:03:31 +0100

    add new book form

--- sql_library_manager/app.js
+++ sql_library_manager/app.js
@@ -22,9 +22,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.get('/', (req, res) => {
-  res.redirect('/books');
-})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- sql_library_manager/routes/index.js
+++ sql_library_manager/routes/index.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.redirect('/books');
-  //res.render('index', { title: 'Express' });
 });
+
+router.get('/books', function(req, res, next){
+  res.render('index', {title: 'Books'});
+})
+
+router.get('/new-book', function(req, res, next) {
+  res.render('new-book', {title: 'New Book'});
+})
 
 module.exports = router;
