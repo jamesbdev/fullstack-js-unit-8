dd60878662d436a5a5ead805dcbc58a4aae15025
James Bremner <j.bremner@ymail.com>
2024-05-31 15:17:14 +0100

    add comments

--- sql_library_manager/models/book.js
+++ sql_library_manager/models/book.js
@@ -1,57 +1,68 @@
-'use strict';
+"use strict";
 
-const { db } = require("index.js");
+const { db } = require("./index.js");
 
 const sequelize = db.sequelize;
 const Sequelize = db.Sequelize;
 
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class Book extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
+const { Model, DataTypes } = require("sequelize");
+
+class Book extends Model {
+  /**
+   * Helper method for defining associations.
+   * This method is not a part of Sequelize lifecycle.
+   * The `models/index` file will call this method automatically.
+   */
+  static associate(models) {
+    // define association here
   }
-  Book.init({
+}
+Book.init(
+  {
     title: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        msg: 'Please enter a value for "title"',
-      }
+        notEmpty: {
+          msg: 'Please enter a value for "title"',
+        }
+       
+      },
     },
     author: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        msg: 'Please enter a value for "author"',
-      }
+        notEmpty: {
+          msg: 'Please enter a value for "author"',
+        }
+      },
     },
     genre: DataTypes.STRING,
-    year: DataTypes.INTEGER
-  }, {
+    year: DataTypes.INTEGER,
+  },
+  {
     sequelize,
-    modelName: 'Book',
+    modelName: "Book",
   });
-  return Book;
-};
 
-(async () => {
-  console.log(123);
-  try {
-    await Book.sync();
-    console.log('Book table has been synchronised');
-    await sequelize.authenticate();
-    console.log('Connection to database successful')
-  } catch(error) {
-    console.log('error connecting to the database', error);
-  }
+  (async () => {
+ 
+    try {
+      await Book.sync();
+      console.log('Book table has been synchronised');
 
-})();
+      await sequelize.authenticate();
+      console.log('Connection to database successful');
+      
+      const books = await Book.findAll();
+      console.log(books); 
+      
+    } catch(error) {
+      console.log('error connecting to the database', error);
+    }
+  
+  }) ();
+  
+
+module.exports = Book;
--- sql_library_manager/routes/index.js
+++ sql_library_manager/routes/index.js
@@ -2,29 +2,44 @@ var express = require('express');
 var router = express.Router();
 
 
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.redirect('/books');
 });
 
+//show home page with list of books
 router.get('/books', function(req, res, next){
   res.render('index', {title: 'Books'});
+  //display list of books
+    //pass in book object
+    //loop through book object entries
+    //display books info
 })
 
-//route for new book form
+//get the new book page
 router.get('/books/new', function(req, res, next) {
   res.render('new-book', {title: 'New Book'});
 })
 
-
+//create book route
 router.post('/books/new', function(req, res, next) {
   //post new book to database
   //create new book entry
 })
 
+//get the update book page
 router.get('/books/:id', function(req, res, next) {
   res.render('update-book', {title: 'Update Book'});
+})
+
+//update book route
+router.post('/books/:id', function(req, res, next) {
+  //update book
+})
+
+//delete book route
+router.post('/books/:id/delete', function(req, res, next) {
+  //delete a book 
 })
 
 module.exports = router;
