3c1f63bbb65e6c429c91df5dec9494115609e697
James Bremner <j.bremner@ymail.com>
2024-05-31 10:08:08 +0100

    add update book form

--- sql_library_manager/routes/index.js
+++ sql_library_manager/routes/index.js
@@ -12,13 +12,19 @@ router.get('/books', function(req, res, next){
   res.render('index', {title: 'Books'});
 })
 
+//route for new book form
 router.get('/books/new', function(req, res, next) {
   res.render('new-book', {title: 'New Book'});
 })
 
+
 router.post('/books/new', function(req, res, next) {
   //post new book to database
   //create new book entry
+})
+
+router.get('/books/:id', function(req, res, next) {
+  res.render('update-book', {title: 'Update Book'});
 })
 
 module.exports = router;
